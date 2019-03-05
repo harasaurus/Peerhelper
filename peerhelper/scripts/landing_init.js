d3871d5d18957d36ea7e951add7c8ee689c88e81
Harshit Pandey <harshit7598@gmail.com>
2019-03-05 18:34:26 +0530

    version H02: commit 2
    -solved bug for landing page
    -added options for showing errors
    -added validation for checking required field

--- peerhelper/scripts/landing_init.js
+++ peerhelper/scripts/landing_init.js
@@ -1,9 +1,103 @@
-function signup_validation(){
+function signup(fname, lname, uname, email, pass, repass){
 
 }
 
-function login_validation(){
+function login(uname, password){
+
+}
+
+function show_error(element_id, message){
+	var element = document.getElementById(element_id);
+	element.innerHTML = message;
+}
+
+function remove_error(element_id){
+	var element = document.getElementById(element_id);
+	element.innerHTML = null;
+}
+
+function remove_errors_signup(){
+	remove_error("signup_fname_error");
+	remove_error("signup_lname_error");
+	remove_error("signup_uname_error");
+	remove_error("signup_email_error");
+	remove_error("signup_pass_error");
+	remove_error("signup_repass_error");
+}
+
+function remove_errors_login(){
+	remove_error("login_uname_error");
+	remove_error("login_pass_error");
+}
+
+function validate_info(fname, lname, uname, email, pass, repass){
+	var validated = true;
+	if(!fname){
+		validated = false;
+		show_error("signup_fname_error", "Field is required");
+	}
+
+	if(!uname){
+		validated = false;
+		show_error("signup_uname_error", "Field is required");
+	}
+
+	if(!email){
+		validated = false;
+		show_error("signup_email_error", "Field is required");
+	}
+
+	if(!pass){
+		validated = false;
+		show_error("signup_pass_error", "Field is required");
+	}
+
+	if(!repass){
+		validated = false;
+		show_error("signup_repass_error", "Field is required");
+	}
+
+	return validated;
+}
+
+function validate_credential(uname, password){
+	var validated = true;
 	
+	if(!uname){
+		validated = false;
+		show_error("login_uname_error", "Field is required");
+	}
+
+	if(!password){
+		validated = false;
+		show_error("login_pass_error", "Field is required");
+	}	
+}
+
+function signup_validation(){
+	var fname = document.getElementById("signup_fname").value;
+	var lname = document.getElementById("signup_lname").value;
+	var uname = document.getElementById("signup_uname").value;
+	var email = document.getElementById("signup_email").value;
+	var pass = document.getElementById("signup_pass").value;
+	var repass = document.getElementById("signup_repass").value;
+
+	remove_errors_signup();
+
+	if(validate_info(fname, lname, uname, email, pass, repass)){
+		signup(fname, lname, uname, email, pass, repass);				
+	}
+}
+
+function login_validation(){
+	var uname = document.getElementById("login_uname").value;
+	var password = document.getElementById("login_pass").value;
+
+	remove_errors_login();
+
+	if(validate_credential(uname, password)){
+		login(uname, password);
+	}
 }
 
 function signup_clicked(){
